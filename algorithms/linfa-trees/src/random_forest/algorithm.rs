e3cb3dec42703b5a64920dfcbd77be1b18da70cb
Jakub Jabłoński <jakub.jablonski8.stud@pw.edu.pl>
2023-12-13 18:07:52 +0100

    Fit framework

--- algorithms/linfa-trees/src/random_forest/algorithm.rs
+++ algorithms/linfa-trees/src/random_forest/algorithm.rs
@@ -1,10 +1,18 @@
 use ndarray::{Array1, ArrayBase, Data, Ix2};
-use linfa::{Float, Label};
+use linfa::{
+    dataset::{Labels, AsSingleTargets},
+    error::Error,
+    error::Result,
+    traits::*,
+    DatasetBase, Float, Label,
+};
+use linfa::prelude::Fit;
 use linfa::traits::PredictInplace;
-use crate::DecisionTree;
+use crate::{DecisionTree, DecisionTreeParams, RandomForestValidParams};
 
 pub struct RandomForestClassifier<F: Float, L: Label> {
     trees: Vec<DecisionTree<F, L>>, // collection of fitted decision trees of the forest
+    oob_score: Option<f32>
 }
 
 impl<F: Float, L: Label + Default, D: Data<Elem = F>> PredictInplace<ArrayBase<D, Ix2>, Array1<L>>
@@ -18,4 +26,74 @@ for RandomForestClassifier<F, L>
         Array1::default(x.nrows())
         // TODO implement
     }
+}
+
+impl<F, L, D, T> RandomForestClassifier<F, L> {
+    fn calculate_oob_score() -> Option<f32> {
+        // TODO implement
+        // TODO correct function signature
+        Float::default()
+    }
+    fn bootstrap(&self, dataset: &DatasetBase<ArrayBase<D, Ix2>, T>, num_trees: i32,
+    max_samples: i32) -> Vec<&DatasetBase<ArrayBase<D, Ix2>, T>>
+        where
+            D: Data<Elem = F>,
+            T: AsSingleTargets<Elem = L> + Labels<Elem = L> {
+        // TODO implement
+        Vec::default()
+    }
+    fn build_trees() -> Vec<DecisionTree<F, T>> {
+        // TODO implement
+        Vec::default()
+    }
+}
+
+impl<'a, F: Float, L: Label + 'a + std::fmt::Debug, D, T> Fit<ArrayBase<D, Ix2>, T, Error>
+for RandomForestValidParams<F, L>
+    where
+        D: Data<Elem = F>,
+        T: AsSingleTargets<Elem = L> + Labels<Elem = L>,
+{
+    type Object = RandomForestClassifier<F, L>;
+
+    fn fit(&self, dataset: &DatasetBase<ArrayBase<D, Ix2>, T>) -> Result<Self::Object> {
+
+        // TODO extend implementation
+        // This is a draft - many things may be changed or added
+
+        if !self.bootstrap && self.oob_score {
+            Err("OOB score is available only with bootstrap")
+        }
+
+        let mut fitted_trees: Vec<DecisionTree<F, T>> = Vec::new();
+        if self.bootstrap {
+            let samples = self.bootstrap(dataset, self.num_trees, self.max_samples.unwrap());
+            for sample in samples {
+                let tree = DecisionTreeParams::from(DecisionTreeParams(
+                    self.trees_parameters.clone()
+                )).fit(sample);
+                fitted_trees.push(tree.unwrap())
+            }
+        }
+        else {
+            for num_tree in 0..self.num_trees {
+                let tree = DecisionTreeParams::from(DecisionTreeParams(
+                    self.trees_parameters.clone()
+                )).fit(dataset);
+                fitted_trees.push(tree.unwrap())
+            }
+        }
+
+        let oob_score ;
+        if self.oob_score {
+            oob_score = RandomForestClassifier::calculate_oob_score()
+        }
+        else { oob_score = None }
+
+        Ok(RandomForestClassifier{
+            trees: fitted_trees,
+            oob_score
+        })
+    }
+
 }
--- algorithms/linfa-trees/src/random_forest/hyperparams.rs
+++ algorithms/linfa-trees/src/random_forest/hyperparams.rs
@@ -2,11 +2,11 @@ use linfa::Float;
 use crate::DecisionTreeValidParams;
 
 pub struct RandomForestValidParams<F, L> {
-    trees_parameters: DecisionTreeValidParams<F, L>,
-    num_trees: i32, // number of estimators
-    bootstrap: bool, // is bootstrapping enabled
-    oob_score: bool, // is oob score enabled
-    max_samples: Option<f32>, // number of samples to bootstrap
+    pub(crate) trees_parameters: DecisionTreeValidParams<F, L>,
+    pub(crate) num_trees: i32, // number of estimators
+    pub(crate) bootstrap: bool, // is bootstrapping enabled
+    pub(crate) oob_score: bool, // is oob score enabled
+    pub(crate) max_samples: Option<i32>, // number of samples to bootstrap
 }
 
 impl<F: Float, L> RandomForestValidParams<F, L> {
@@ -22,7 +22,7 @@ impl<F: Float, L> RandomForestValidParams<F, L> {
     pub fn set_oob_score(&mut self, oob_score: bool) {
         self.oob_score = oob_score;
     }
-    pub fn set_max_samples(&mut self, max_samples: Option<f32>) {
+    pub fn set_max_samples(&mut self, max_samples: Option<i32>) {
         self.max_samples = max_samples;
     }
 }
